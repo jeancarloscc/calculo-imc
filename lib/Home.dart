7b66b3a2d5c0c98f64266e915a303f14e3c0bfea
Jean Carlos <jeancc.costa@gmail.com>
2025-02-04 17:23:58 -0300

    atualização de algumas informações

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -14,21 +15,31 @@ class _HomeState extends State<Home> {
   String resultadoIMC = "";
   String pesoIdeal = "";
 
-  int _opcaoEscolhida;
+  int _opcaoEscolhida = 0;
 
-  _calcular() {
-    double peso = double.tryParse(_peso.text);
-    double altura = double.tryParse(_altura.text);
+  void _calcular() {
+    double? peso = double.tryParse(_peso.text);
+    double? altura = double.tryParse(_altura.text);
+
+    if (peso == null || altura == null || altura == 0) {
+      // Trate o caso em que peso ou altura são nulos ou altura é zero
+      setState(() {
+        exibirResultado = "Por favor, insira valores válidos para peso e altura.";
+      });
+      return;
+    }
+
     double resultado = peso / pow(altura, 2);
     _tabelaIMC(resultado);
+
     altura = altura * 100;
 
-    if(_opcaoEscolhida == 0){
+    if (_opcaoEscolhida == 0) {
       double result = (altura - 100) * 0.90;
-      pesoIdeal = "Seu Peso Ideal: " + result.toString() + " Kg";
+      pesoIdeal = "Seu Peso Ideal: " + result.toStringAsFixed(2) + " Kg";
     } else {
       double result = (altura - 100) * 0.85;
-      pesoIdeal = "Seu Peso Ideal: " + result.toString() + " Kg";
+      pesoIdeal = "Seu Peso Ideal: " + result.toStringAsFixed(2) + " Kg";
     }
 
     setState(() {
@@ -39,17 +50,17 @@ class _HomeState extends State<Home> {
   String _tabelaIMC(double resultado) {
     if (resultado < 16) {
       resultadoIMC = "Baixo peso muito grave";
-    } else if (resultado > 16 && resultado < 16.99) {
+    } else if (resultado >= 16 && resultado < 16.99) {
       resultadoIMC = "Baixo peso grave";
-    } else if (resultado > 17 && resultado < 18.49) {
+    } else if (resultado >= 17 && resultado < 18.49) {
       resultadoIMC = "Baixo Peso";
-    } else if (resultado > 18.50 && resultado < 24.99) {
+    } else if (resultado >= 18.50 && resultado < 24.99) {
       resultadoIMC = "Peso normal";
-    } else if (resultado > 25 && resultado < 29.99) {
+    } else if (resultado >= 25 && resultado < 29.99) {
       resultadoIMC = "Sobrepeso";
-    } else if (resultado > 30 && resultado < 34.99) {
+    } else if (resultado >= 30 && resultado < 34.99) {
       resultadoIMC = "Obesidade grau 1";
-    } else if (resultado > 35 && resultado < 39.99) {
+    } else if (resultado >= 35 && resultado < 39.99) {
       resultadoIMC = "Obesidade grau 2";
     } else {
       resultadoIMC = "Obesidade grau 3 (obesidade mórbida)";
@@ -58,8 +69,8 @@ class _HomeState extends State<Home> {
   }
 
   void _resetFields() {
-    _altura.text="";
-    _peso.text="";
+    _altura.text = "";
+    _peso.text = "";
     setState(() {
       _formKey = GlobalKey<FormState>();
       exibirResultado = "";
@@ -72,124 +83,137 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Cálcular o IMC"),
+        title: Text("Calcular o IMC"),
         actions: [
-          IconButton(icon: Icon(Icons.refresh), onPressed: _resetFields,)
+          IconButton(icon: Icon(Icons.refresh), onPressed: _resetFields)
         ],
       ),
       body: Container(
-          decoration: BoxDecoration(
-          ),
-          height: double.infinity,
-          width: double.infinity,
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Text(
-                      "Seu Indice de Massa Corporal é:",
-                      style: TextStyle(fontSize: 24),
-                    ),
+        decoration: BoxDecoration(),
+        height: double.infinity,
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    "Seu Índice de Massa Corporal é:",
+                    style: TextStyle(fontSize: 24),
                   ),
-                  Divider(),
-                  Text(exibirResultado, style: TextStyle(fontSize: 20), textAlign: TextAlign.center,),
-                  Text(resultadoIMC + " / " + pesoIdeal, style: TextStyle(fontSize: 18), textAlign: TextAlign.center,),
-                  Divider(),
-                  Form(
-                    key: _formKey,
-                    child: Column(
-                      children: <Widget>[
-                        TextFormField(
-                          controller: _altura,
-                          keyboardType: TextInputType.number,
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return "Preencha o campo";
-                            } else if(value.contains(",")) {
-                              return "Coloque o '.' no valor";
-                            }
-                            return null;
-                          },
-                          decoration: InputDecoration(
-                              hintText: "Ex:. 1.70",
-                              filled: true,
-                              labelText: "Altura (m)",
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              )),
-                        ),
-                        Divider(),
-                        TextFormField(
-                          controller: _peso,
-                          keyboardType: TextInputType.number,
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return "Preencha o campo";
-                            } else if(value.contains(",")) {
-                              return "Coloque o '.' no valor";
-                            }
-                            return null;
-                          },
-                          decoration: InputDecoration(
-                              hintText: "Ex:. 75.2",
-                              filled: true,
-                              labelText: "Peso (kg)",
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              )),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Divider(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Radio(
-                          value: 0,
-                          groupValue: _opcaoEscolhida,
-                          onChanged: (int value){
-                            setState(() {
-                              _opcaoEscolhida = value;
-                            });
+                ),
+                Divider(),
+                Text(
+                  exibirResultado,
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  resultadoIMC + " / " + pesoIdeal,
+                  style: TextStyle(fontSize: 18),
+                  textAlign: TextAlign.center,
+                ),
+                Divider(),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        controller: _altura,
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Preencha o campo";
+                          } else if (value.contains(",")) {
+                            return "Coloque o '.' no valor";
                           }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                            hintText: "Ex:. 1.70",
+                            filled: true,
+                            labelText: "Altura (m)",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            )),
                       ),
-                      Text("Masculino", style: TextStyle(fontSize: 20),),
-                      Radio(
-
-                          value: 1,
-                          groupValue: _opcaoEscolhida,
-                          onChanged: (int value){
-                            setState(() {
-                              _opcaoEscolhida = value;
-                            });
+                      Divider(),
+                      TextFormField(
+                        controller: _peso,
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Preencha o campo";
+                          } else if (value.contains(",")) {
+                            return "Coloque o '.' no valor";
                           }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                            hintText: "Ex:. 75.2",
+                            filled: true,
+                            labelText: "Peso (kg)",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            )),
                       ),
-                      Text("Feminino", style: TextStyle(fontSize: 20),),
                     ],
                   ),
-                  Divider(),
-                  Container(
-                    padding: EdgeInsets.only(left: 20, right: 20),
-                    height: 50,
-                    child: FilledButton(
-                      onPressed: () {
-                        if(_formKey.currentState.validate()) {
-                          _calcular();
-                        }
-                      },
-                      child: Text("Calcular", style: TextStyle(fontSize: 20),),
-                      // color: Colors.white,
+                ),
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Radio(
+                        value: 0,
+                        groupValue: _opcaoEscolhida,
+                        onChanged: (int? value) {
+                          setState(() {
+                            _opcaoEscolhida = value!;
+                          });
+                        }),
+                    Text(
+                      "Masculino",
+                      style: TextStyle(fontSize: 20),
                     ),
-                  )
-                ],
-              ),
-            )
-          )),
+                    Radio(
+                        value: 1,
+                        groupValue: _opcaoEscolhida,
+                        onChanged: (int? value) {
+                          setState(() {
+                            _opcaoEscolhida = value!;
+                          });
+                        }),
+                    Text(
+                      "Feminino",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+                Divider(),
+                Container(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  height: 50,
+                  child: FilledButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _calcular();
+                      }
+                    },
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
