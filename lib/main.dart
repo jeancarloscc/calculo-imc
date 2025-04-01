788ca688d9204f4c0f45e8ed2f2b32a1e76cd4ef
Jean Carlos <jeancc.costa@gmail.com>
2025-04-01 12:39:45 -0300

    pequenas atualizações no main

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,21 @@
 
 import 'package:flutter/material.dart';
 import 'package:imc_calculo/color_schemes.g.dart';
+import 'package:imc_calculo/controller/IMCController.dart';
 import 'presentation/pages/home_page.dart';
 import 'core/usecases/calcular_imc.dart';
-import 'data/repositories/imc_repository.dart';
 
 void main() {
-  final IMCRepository imcRepository = IMCRepository();
-  final CalcularIMC calcularIMC = CalcularIMC(imcRepository);
+  final calcularIMC = CalcularIMC();
+  final controller = IMCController(calcularIMC);
 
-  runApp(MyApp(calcularIMC: calcularIMC));
+  runApp(MyApp(controller: controller));
 }
 
 class MyApp extends StatelessWidget {
-  final CalcularIMC calcularIMC;
+  final IMCController controller;
 
-  MyApp({required this.calcularIMC});
+  const MyApp({required this.controller, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       title: 'Calculadora de IMC',
       theme: ThemeData.from(colorScheme: lightColorScheme),
       darkTheme: ThemeData.from(colorScheme: darkColorScheme),
-      home: HomePage(calcularIMC: calcularIMC),
+      home: HomePageTeste(controller: controller,),
     );
   }
 }
