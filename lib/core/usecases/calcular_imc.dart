2e16cc24829247f088de22e490882888eb8a9964
Jean Carlos <jeancc.costa@gmail.com>
2025-04-01 12:39:16 -0300

    foi colocado argumentos para erro

--- lib/core/usecases/calcular_imc.dart
+++ lib/core/usecases/calcular_imc.dart
@@ -1,13 +1,6 @@
-// lib/core/usecases/calcular_imc.dart
-
-import 'package:imc_calculo/data/repositories/imc_repository.dart';
-
 class CalcularIMC {
-  final IMCRepository imcRepository;
-
-  CalcularIMC(this.imcRepository);
-
   double executar(double peso, double altura) {
-    return imcRepository.calcularIMC(peso, altura);
+    if (altura <= 0) throw ArgumentError("Altura deve ser maior que zero.");
+    return peso / (altura * altura);
   }
 }
