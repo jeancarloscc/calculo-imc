a28547ba6569a38832c94676a0e9e970ea41b1ff
Jean Carlos <jeancc.costa@gmail.com>
2025-04-04 21:22:40 -0300

    Remove arquivos ignorados

--- android/app/src/main/kotlin/com/jean/dev/imc_calculo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.jean.dev.imccalculo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/core/usecases/calcular_imc.dart
+++
@@ -1,6 +0,0 @@
-class CalcularIMC {
-  double executar(double peso, double altura) {
-    if (altura <= 0) throw ArgumentError("Altura deve ser maior que zero.");
-    return peso / (altura * altura);
-  }
-}
